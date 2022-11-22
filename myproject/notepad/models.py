218424868378d3019c9178c7480b88be61d05c9f
Samu Syrjänen <samu.syrjanen@gmail.com>
2022-11-22 23:28:29 +0200

    created project and app

---
+++ myproject/notepad/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Account(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Note(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    input_text = models.CharField(max_length=200)
