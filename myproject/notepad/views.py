88b087a56c3ca5ec1a15aa846b0cb85ad865d343
Samu Syrjänen <samu.syrjanen@gmail.com>
2022-11-23 19:20:11 +0200

    url paths to index and notes

--- myproject/notepad/views.py
+++ myproject/notepad/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the notepads index.")#.html template here. like this:  return render(request, 'polls/detail.html', {'question': question})
+
+def notes(request):
+    return HttpResponse('you are at the note page')
