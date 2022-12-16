35e69c61c14ceeca8a23c59fc646c3d8216da789
Samu Syrjänen <samu.syrjanen@gmail.com>
2022-12-16 16:13:49 +0200

    modified comments

--- myproject/myproject/create_session.py
+++ myproject/myproject/create_session.py
@@ -9,20 +9,9 @@ class SessionStore(db.SessionStore):
 	def _get_new_session_key(self):
 		while True:
 			session_key = 'session-' + str(SessionStore.session_counter)#session hijacking can be done with sessionid if sessionid is easy to guess
+            #solution for session hijacking:
+            #this generates a random sessionid instead of "session-<int>"
+            #session_key = str(random.getrandbits(128)) + str(SessionStore.session_counter)
 			SessionStore.session_counter += 1
 			if not self.exists(session_key):
 				return session_key
-
-#solution for session hijacking
-#this generates a random sessionid instead of "session-<int>"
-'''
-class SessionStore(db.SessionStore):
-	session_counter = 0
-
-	def _get_new_session_key(self):
-		while True:
-			session_key = str(random.getrandbits(128)) + str(SessionStore.session_counter)
-			SessionStore.session_counter += 1
-			if not self.exists(session_key):
-				return session_key
-'''
