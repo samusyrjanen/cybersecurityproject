46a754763f03b027f81a3ceae3662ee77f3cc4a1
Samu Syrjänen <samu.syrjanen@gmail.com>
2022-12-16 12:54:37 +0200

    editing comments

--- myproject/notepad/database_reader.py
+++ myproject/notepad/database_reader.py
@@ -11,31 +11,23 @@ def get_all_notes():
 
 def get_user_notes(username):
     cursor = connection.cursor()
-    sql = f'select content, username from notes where username="{username}"' #SQL injection
+    sql = f'select content, username from notes where username="{username}"' #SQL injection possible
     cursor.execute(sql)
-    rows = cursor.fetchall()
-    return [(row['content'], row['username']) for row in rows]
-
-def write_note(content, username):
-    print(type(username), type(content))
-    cursor = connection.cursor()
-    sql = f'insert into notes (content, username) values ("{content}", "{username}")' #SQL injection
-    cursor.execute(sql)
-    connection.commit()
-
-#This is the correct way to make SQL queries:
-'''
-def get_user_notes(username):
-    cursor = connection.cursor()
+    #This is the correct way to make SQL queries:
+    '''
     sql = 'select content, username from notes where username=:username'
     cursor.execute(sql, {'username': username})
+    '''
     rows = cursor.fetchall()
     return [(row['content'], row['username']) for row in rows]
 
 def write_note(content, username):
-    print(type(username), type(content))
     cursor = connection.cursor()
+    sql = f'insert into notes (content, username) values ("{content}", "{username}")' #SQL injection possible
+    cursor.execute(sql)
+    #This is the correct way to make SQL queries:
+    '''
     sql = 'insert into notes (content, username) values (:content, :username)'
     cursor.execute(sql, {'content': content, 'username': username})
+    '''
     connection.commit()
-'''
